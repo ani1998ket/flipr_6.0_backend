6d7d008650bb9e12023c1bb5528111f2f5f993b3
aniket <ani1998ket@gmail.com>
2020-09-14 19:19:44 +0530

    Added default query

--- server.js
+++ server.js
@@ -19,7 +19,22 @@ app.get("/api/test", (req, res, next) => {
     req.query.age_min = parseInt( req.query.age_min )
     req.query.age_max = parseInt( req.query.age_max )
     let p = req.query;
-    var sql = `SELECT * FROM covid19india where ageEstimate + 0 >= ${p.age_min} AND ageEstimate + 0 <= ${p.age_max} AND gender = "${p.gender}" AND STATE = "${p.state}" AND (substr(reportedOn, 7, 4) || '-' || substr(reportedOn, 4, 2) || '-' || substr(reportedOn, 1, 2)) between "${p.date_start}" and "${p.date_end}" `; 
+
+    if(p.age_max == 0) p.age_max = 100;
+    if( p.date_start == "" ) p.date_start = "2020-01-01";
+    if( p.date_end == "" )p.date_end = "2020-12-31";
+
+    let age_query = ` AND ageEstimate + 0 >= ${p.age_min} AND ageEstimate + 0 <= ${p.age_max}`;
+    let gender_query = ` AND gender = "${p.gender}"`;
+    let state_query = ` AND STATE = "${p.state}"`;
+    let date_query = ` AND (substr(reportedOn, 7, 4) || '-' || substr(reportedOn, 4, 2) || '-' || substr(reportedOn, 1, 2)) between "${p.date_start}" and "${p.date_end}" `;
+
+    var sql = `SELECT patientId, reportedOn, ageEstimate, gender, state, status FROM covid19india where 1`;
+    if( p.gender != "") sql += gender_query;
+    if( p.state != "") sql += state_query;
+    sql += age_query;
+    sql += date_query;
+
     console.log( sql )
     var params = []
     db.all(sql, params, (err, rows) => {
